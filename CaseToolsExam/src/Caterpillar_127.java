adaa394497e7ab95e1017fa82cc71a4c2d348f77
admin <admin@Bcalab4-m06>
2022-06-14 09:21:11 +0530

    commit

---
+++ CaseToolsExam/src/Caterpillar_127.java
@@ -0,0 +1,14 @@
+
+public class Caterpillar_127 {
+	int moves,cabbages;
+	Caterpillar_127(int moves, int cabbage)
+	{
+		this.moves=moves;
+		this.cabbages=cabbages;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
