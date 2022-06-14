1b2eadc4689d00e9970bfb01104e6c3632a3d6f2
Kahlil7 <100745886+Kahlil7@users.noreply.github.com>
2022-06-14 09:38:04 +0530

    Update Caterpillar_127.java

--- CaseToolsExam/src/Caterpillar_127.java
+++ CaseToolsExam/src/Caterpillar_127.java
@@ -6,6 +6,11 @@ public class Caterpillar_127 {
 		this.moves=moves;
 		this.cabbages=cabbages;
 	}
+	
+	int get_moves()
+	{
+		return moves;
+	}
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
